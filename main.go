7bca89ef75382ca566c3a73e09d0c533a7579569
Denis Mishankov <mishankov@mail.com>
2026-09-18 16:07:15 +0300

    Improve release packaging experience (#9)

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Initialisation error:", err)
 		return
 	}
-	err = wails.Run(&options.App{Title: "Camunda Stub Worker", Width: 1280, Height: 820, MinWidth: 1024, MinHeight: 700, BackgroundColour: &options.RGBA{R: 14, G: 18, B: 25, A: 1}, AssetServer: &assetserver.Options{Assets: assets}, OnStartup: app.startup, OnShutdown: app.shutdown, OnBeforeClose: app.beforeClose, Bind: []interface{}{app}, Linux: &linux.Options{Icon: appIcon, ProgramName: "camunda-stub-worker"}})
+	err = wails.Run(&options.App{Title: "Camunda Stub Worker", Width: 1280, Height: 820, MinWidth: 1024, MinHeight: 700, BackgroundColour: &options.RGBA{R: 14, G: 18, B: 25, A: 1}, AssetServer: &assetserver.Options{Assets: assets}, OnStartup: app.startup, OnShutdown: app.shutdown, OnBeforeClose: app.beforeClose, Bind: []interface{}{app}, Linux: &linux.Options{Icon: appIcon, ProgramName: "Camunda Stub Worker"}})
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Startup error:", err)
 	}
